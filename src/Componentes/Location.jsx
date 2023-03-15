c4c53c6963cea91da2a566d6a5419bfbb09f3703
kevin villarruel <kevinevolutivos@gmail.com>
2023-03-15 17:29:41 -0500

    Proyec2

--- src/Componentes/Location.jsx
+++ src/Componentes/Location.jsx
@@ -3,18 +3,18 @@ import React from 'react';
 const Location = ({ name, type, dimension, residents }) => {
   return (
     <section className=" bg-emerald-900 ">
-      <h2 className="text-center">{name}</h2>
+      <h2 className="text-center text-lime-400 text-5xl ">{name}</h2>
       <ul className=" lg:text-2xl justify-center lg:py-16 lg:grid grid-cols-3 px-60 sm:flex flex-col ">
         <li className="flex flex-col m-3 ">
-          <span>Type: </span>
+          <span className='text-lime-500'>Type: </span>
           {type}
         </li>
         <li className="flex flex-col m-3">
-          <span>dimension: </span>
+          <span className='text-lime-500'>dimension: </span>
           {dimension}
         </li>
         <li className="flex flex-col m-3">
-          <span>Population: </span>
+          <span className='text-lime-500'>Population: </span>
           {residents.length}
         </li>
       </ul>
